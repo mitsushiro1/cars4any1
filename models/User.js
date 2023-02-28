f23c20956cf214c821a852bf5d8cf94b35e541f9
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-28 16:26:21 -0500

    added data models

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8]
+      },
+    },
+    user_city_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'city',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
---
+++ models/Vehicle.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vehicle  extends Model {};
+
+Vehicle.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    is_luxury: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    body_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fuel_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    seating_capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 5
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vehicle'
+  }
+);
+
+module.exports = Vehicle;
