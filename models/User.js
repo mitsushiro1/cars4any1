6cffc8b3e6e656cadcc017dc3714102861b8b4ea
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-03-02 12:37:53 -0500

    added post routes and changed user.js to have bcrypt

--- models/User.js
+++ models/User.js
@@ -48,6 +48,16 @@ User.init(
     }
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+  },
     sequelize,
     timestamps: true,
     freezeTableName: true,
