f44f67de91fb4038dd0407402cb62d40d644cf22
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-03 12:13:09 -0500

    finished profile page update features

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,12 +1,13 @@
 const updateEmailBtn = document.getElementById('update-email');
 const CE_close = document.getElementById('CE-close');
+const CA_yes = document.getElementById('CA-yes');
 const CA_no = document.getElementById('CA-no');
 const updateAccountBtn = document.getElementById('update-account');
 
 const CE_update = document.getElementById('CE-update');
-const newEmail = document.getElementById('new-email').value;
 
 const userid = parseInt(document.getElementById('profile-userid').innerText);
+
 
 updateEmailBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -35,10 +36,36 @@ CA_no.addEventListener('click', (e) => {
 CE_update.addEventListener('click', async (e) => {
   e.preventDefault();
   e.stopPropagation();
+  const newEmail = document.querySelector('.change-email #new-email').value;
+  console.log(newEmail);
   const response = await fetch(`/api/users/${userid}`, {
     method: 'PATCH',
     body: JSON.stringify({
       email: newEmail
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    await fetch('/api/users/logout', {
+      method: 'POST'
+    });
+    document.location.reload();
+  } else {
+    alert('Something went wrong, please try again');
+  }
+});
+
+CA_yes.addEventListener('click', async (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const newValue = document.querySelector('#profile-is-vendor').innerText === 'false' ? true : false;
+  console.log(newValue);
+
+  const response = await fetch(`/api/users/${userid}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      is_vendor: newValue
     }),
     headers: { 'Content-Type': 'application/json' }
   });
