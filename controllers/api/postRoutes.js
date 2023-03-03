10825fdc81232ed547da6c9b93e96cb6cb882ddf
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-03 10:51:01 -0500

    working on posting view

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -57,7 +57,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const postData = await Post.create(req.body);
+    const postData = await Posting.create(req.body);
     console.log(req.body);
     res.status(200).json(postData);
   } catch (err) {
